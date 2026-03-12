76bb8cbd34d59a921b03b34dd9088066a49a506b
pasbra <p.braccolino@orange.fr>
2026-03-12 16:48:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def nettoyer_code(code_saisi):
 # Fonction pour rechercher un code dans code.txt
 def rechercher_code(code_nettoye, fichier_table="code.txt"):
     try:
-       with open(fichier_table, 'r', encoding='latin-1') as f:
+       wwith open(fichier_table, 'r', encoding='cp1252') as f:
             lignes = f.readlines()
         for ligne in lignes:
             elements = ligne.strip().split(';')
